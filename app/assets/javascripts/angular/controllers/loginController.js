8971bd7ff53d10058908bf5b4550fd93c0d0977d
Christian Vuerings <vueringschristian@gmail.com>
2013-04-12 17:06:41 -0700

    add basic auth

---
+++ app/assets/javascripts/angular/controllers/loginController.js
@@ -0,0 +1,33 @@
+(function() {
+  'use strict';
+
+  /**
+   * Activity controller
+   */
+  calcentral.controller('LoginController', ['$http', '$scope', function($http, $scope) {
+
+    //$http.defaults.headers.common.Authorization = 'Basic ' + Base64.encode($scope.login.login + ':' + $scope.login.password);
+
+    $scope.login = {};
+    $scope.login.user = null;
+
+    $scope.login.connect = function() {
+        $http.get('/basic_auth_login').success(function(data, status) {
+            if (status < 200 || status >= 300) {
+              return;
+            }
+            $scope.login.user = data;
+        });
+    };
+
+    $scope.login.disconnect = function() {
+        $scope.login.user = null;
+    };
+
+    $scope.$watch('login.login + login.password', function() {
+        $http.defaults.headers.common.Authorization = 'Basic ' + Base64.encode($scope.login.login + ':' + $scope.login.password);
+    });
+
+  }]);
+
+})();
