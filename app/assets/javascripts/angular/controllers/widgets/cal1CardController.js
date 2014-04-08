dc8bc2b631adc45e037c17a25379c7050b8a6571
Christian Vuerings <vueringschristian@gmail.com>
2014-04-07 17:14:53 -0700

    CLC-3517 - WIP

---
+++ app/assets/javascripts/angular/controllers/widgets/cal1CardController.js
@@ -0,0 +1,19 @@
+(function(angular) {
+  'use strict';
+
+  /**
+   * Footer controller
+   */
+  angular.module('calcentral.controllers').controller('Cal1CardController', function(cal1CardFactory, $scope) {
+
+    var loadCal1Card = function() {
+      cal1CardFactory.getCal1Card().success(function(data) {
+        angular.extend($scope, data);
+      });
+    };
+
+    loadCal1Card();
+
+  });
+
+})(window.angular);
