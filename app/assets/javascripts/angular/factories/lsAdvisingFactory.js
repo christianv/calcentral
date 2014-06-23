78cf2b2320b4bec310c137b7afe5604a4100cba6
Christian Vuerings <vueringschristian@gmail.com>
2014-06-23 16:35:40 -0700

    CLC-3916 - Prototype of L&S advisor appointment data https://jira.ets.berkeley.edu/jira/browse/CLC-3916

---
+++ app/assets/javascripts/angular/factories/lsAdvisingFactory.js
@@ -0,0 +1,21 @@
+(function(angular) {
+
+  'use strict';
+
+  /**
+   * L & S Advising Factory - get data from the bHive API
+   * @param {Object} $http The $http service from Angular
+   */
+  angular.module('calcentral.factories').factory('lsAdvisingFactory', function($http) {
+
+    var getAdvisingInfo = function() {
+      return $http.get('/dummy/json/lsadvising2.json');
+    };
+
+    return {
+      getAdvisingInfo: getAdvisingInfo
+    };
+
+  });
+
+}(window.angular));
