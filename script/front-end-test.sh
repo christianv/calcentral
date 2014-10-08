20822ae9d46adb32524839f8c64cfbe2881fa322
Christian Vuerings <vueringschristian@gmail.com>
2014-10-07 18:39:54 -0700

    test22

--- script/front-end-test.sh
+++ script/front-end-test.sh
@@ -2,4 +2,4 @@ npm -g install jshint
 npm -g install jscs
 gem install scss-lint
 scss-lint app/assets/stylesheets/
-[[ $? -ne 0 ]] && exit 99 || echo 'scss-lint was succesful'
+[[ $? -ne 0 ]] && echo 'scss-lint failed' && exit 1 || echo 'scss-lint was succesful'
