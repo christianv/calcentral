e787dab0ced74474bd41133f54ad39f9a935e449
Christian Vuerings <vueringschristian@gmail.com>
2015-01-26 15:18:53 -0800

    test

--- script/front-end-test.sh
+++ script/front-end-test.sh
@@ -1,4 +1,6 @@
-npm install
+node_version=`node --version`
+echo "Node version: $node_version"
+npm install -verbose
 gem install scss-lint --version 0.31.0
 scss-lint src/assets/stylesheets/
 [[ $? -ne 0 ]] && echo 'scss-lint failed' && exit 1 || echo 'scss-lint was succesful'
