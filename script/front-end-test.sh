fd14a3a5e80736e40f6fcd365d7c48c15f54a7e8
Christian Vuerings <vueringschristian@gmail.com>
2015-01-27 15:28:29 -0800

    Don't run the gulp command globally, use `npm scripts` instead

--- script/front-end-test.sh
+++ script/front-end-test.sh
@@ -3,7 +3,7 @@ echo "Node version: $node_version"
 npm config set strict-ssl false
 npm install
 export CALCENTRAL_WATCH="false"
-gulp build
+npm run build
 gem install scss-lint --version 0.31.0
 scss-lint src/assets/stylesheets/
 [[ $? -ne 0 ]] && echo 'scss-lint failed' && exit 1 || echo 'scss-lint was succesful'
