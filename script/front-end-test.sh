3226dfde019e630ed40edebe8b59dd8d4d922cd0
Christian Vuerings <vueringschristian@gmail.com>
2015-01-27 10:37:07 -0800

    Add `gulp build` to the `front-end-test.sh` script

--- script/front-end-test.sh
+++ script/front-end-test.sh
@@ -1,6 +1,7 @@
 node_version=`node --version`
 echo "Node version: $node_version"
 npm install
+gulp build
 gem install scss-lint --version 0.31.0
 scss-lint src/assets/stylesheets/
 [[ $? -ne 0 ]] && echo 'scss-lint failed' && exit 1 || echo 'scss-lint was succesful'
