052f817d00dc8960f8ff0a6c1624a69baf250e95
Christian Vuerings <vueringschristian@gmail.com>
2015-04-28 16:37:05 -0700

    test

--- src/assets/javascripts/angular/controllers/widgets/finaidBudgetController.js
+++ src/assets/javascripts/angular/controllers/widgets/finaidBudgetController.js
@@ -4,7 +4,12 @@
   /**
    * Financial Aid Budget controller
    */
-  angular.module('calcentral.controllers').controller('FinaidBudgetController', function(finaidFactory, $scope) {
+  angular.module('calcentral.controllers').controller('FinaidBudgetController', function(finaidFactory, finaidTermService, $scope) {
+
+    $scope.$on('calcentral.finaid.term', function() {
+      $scope.budget = finaidTermService.findTerm($scope.finaidBudget.terms);
+    });
+
     var getFinaidActivity = function() {
       finaidFactory.getBudget().success(function(data) {
         angular.extend($scope, data);
